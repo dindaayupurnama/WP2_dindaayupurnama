f85d2380a12ec773d1881ebddd350666d6157dd7
dindaayupurnama <dinda.ayu030402@gmail.com>
2021-09-30 12:06:31 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya dinda ayu purnama saya tinggal di pontianak 
+        olahraga yang saya sukai adalah bola basket";
+
+    }
+}
